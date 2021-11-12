af52cebbcc622aa4c88a535f076873660b385ef6
Eduard Aksamitov <e@edaks.co>
2021-11-12 23:40:10 +0300

    fix: exports

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-export {
-  default as autoLink,
-  default as autoLinkCashtags,
-  default as autoLinkEntities,
-  default as autoLinkHashtags,
-  default as autoLinkUrlsCustom,
-  default as autoLinkUsernamesOrLists,
-  default as autoLinkWithJSON,
-  default as configs,
-  default as convertUnicodeIndices,
-  default as extractCashtags,
-  default as extractCashtagsWithIndices,
-  default as extractEntitiesWithIndices,
-  default as extractHashtags,
-  default as extractHashtagsWithIndices,
-  default as extractHtmlAttrsFromOptions,
-  default as extractMentions,
-  default as extractMentionsOrListsWithIndices,
-  default as extractMentionsWithIndices,
-  default as extractReplies,
-  default as extractUrls,
-  default as extractUrlsWithIndices,
-  default as getTweetLength,
-  default as getUnicodeTextLength,
-  default as hasInvalidCharacters,
-  default as hitHighlight,
-  default as htmlEscape,
-  default as isInvalidTweet,
-  default as isValidHashtag,
-  default as isValidList,
-  default as isValidTweetText,
-  default as isValidUrl,
-  default as isValidUsername,
-  default as linkTextWithEntity,
-  default as linkToCashtag,
-  default as linkToHashtag,
-  default as linkToMentionAndList,
-  default as linkToText,
-  default as linkToTextWithSymbol,
-  default as linkToUrl,
-  default as modifyIndicesFromUTF16ToUnicode,
-  default as modifyIndicesFromUnicodeToUTF16,
-  default as regexen,
-  default as removeOverlappingEntities,
-  default as parseTweet,
-  default as splitTags,
-  default as standardizeIndices,
-  default as tagAttrs
-} from './twitter-text/js/src/index.js'
+export { default as autoLink } from './twitter-text/js/src/autoLink.js'
+export { default as autoLinkCashtags } from './twitter-text/js/src/autoLinkCashtags.js'
+export { default as autoLinkEntities } from './twitter-text/js/src/autoLinkEntities.js'
+export { default as autoLinkHashtags } from './twitter-text/js/src/autoLinkHashtags.js'
+export { default as autoLinkUrlsCustom } from './twitter-text/js/src/autoLinkUrlsCustom.js'
+export { default as autoLinkUsernamesOrLists } from './twitter-text/js/src/autoLinkUsernamesOrLists.js'
+export { default as autoLinkWithJSON } from './twitter-text/js/src/autoLinkWithJSON.js'
+export { default as configs } from './twitter-text/js/src/configs.js'
+export { default as convertUnicodeIndices } from './twitter-text/js/src/convertUnicodeIndices.js'
+export { default as extractCashtags } from './twitter-text/js/src/extractCashtags.js'
+export { default as extractCashtagsWithIndices } from './twitter-text/js/src/extractCashtagsWithIndices.js'
+export { default as extractEntitiesWithIndices } from './twitter-text/js/src/extractEntitiesWithIndices.js'
+export { default as extractHashtags } from './twitter-text/js/src/extractHashtags.js'
+export { default as extractHashtagsWithIndices } from './twitter-text/js/src/extractHashtagsWithIndices.js'
+export { default as extractHtmlAttrsFromOptions } from './twitter-text/js/src/extractHtmlAttrsFromOptions.js'
+export { default as extractMentions } from './twitter-text/js/src/extractMentions.js'
+export { default as extractMentionsOrListsWithIndices } from './twitter-text/js/src/extractMentionsOrListsWithIndices.js'
+export { default as extractMentionsWithIndices } from './twitter-text/js/src/extractMentionsWithIndices.js'
+export { default as extractReplies } from './twitter-text/js/src/extractReplies.js'
+export { default as extractUrls } from './twitter-text/js/src/extractUrls.js'
+export { default as extractUrlsWithIndices } from './twitter-text/js/src/extractUrlsWithIndices.js'
+export { default as getTweetLength } from './twitter-text/js/src/getTweetLength.js'
+export { default as getUnicodeTextLength } from './twitter-text/js/src/getUnicodeTextLength.js'
+export { default as hasInvalidCharacters } from './twitter-text/js/src/hasInvalidCharacters.js'
+export { default as hitHighlight } from './twitter-text/js/src/hitHighlight.js'
+export { default as htmlEscape } from './twitter-text/js/src/htmlEscape.js'
+export { default as isInvalidTweet } from './twitter-text/js/src/isInvalidTweet.js'
+export { default as isValidHashtag } from './twitter-text/js/src/isValidHashtag.js'
+export { default as isValidList } from './twitter-text/js/src/isValidList.js'
+export { default as isValidTweetText } from './twitter-text/js/src/isValidTweetText.js'
+export { default as isValidUrl } from './twitter-text/js/src/isValidUrl.js'
+export { default as isValidUsername } from './twitter-text/js/src/isValidUsername.js'
+export { default as linkTextWithEntity } from './twitter-text/js/src/linkTextWithEntity.js'
+export { default as linkToCashtag } from './twitter-text/js/src/linkToCashtag.js'
+export { default as linkToHashtag } from './twitter-text/js/src/linkToHashtag.js'
+export { default as linkToMentionAndList } from './twitter-text/js/src/linkToMentionAndList.js'
+export { default as linkToText } from './twitter-text/js/src/linkToText.js'
+export { default as linkToTextWithSymbol } from './twitter-text/js/src/linkToTextWithSymbol.js'
+export { default as linkToUrl } from './twitter-text/js/src/linkToUrl.js'
+export { default as modifyIndicesFromUTF16ToUnicode } from './twitter-text/js/src/modifyIndicesFromUTF16ToUnicode.js'
+export { default as modifyIndicesFromUnicodeToUTF16 } from './twitter-text/js/src/modifyIndicesFromUnicodeToUTF16.js'
+export { default as regexen } from './twitter-text/js/src/regexp/index.js'
+export { default as removeOverlappingEntities } from './twitter-text/js/src/removeOverlappingEntities.js'
+export { default as parseTweet } from './twitter-text/js/src/parseTweet.js'
+export { default as splitTags } from './twitter-text/js/src/splitTags.js'
+export { default as standardizeIndices } from './twitter-text/js/src/standardizeIndices.js'
+export { default as tagAttrs } from './twitter-text/js/src/tagAttrs.js'
